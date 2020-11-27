6f36da738e75ecfb8a597b9f3153a4dd6c37c093
Allister Grange <allistergrange@gmail.com>
2020-11-28 08:06:34 +1300

    Added a message if no sounds were playing

--- src/components/VolumeBottomSheet.tsx
+++ src/components/VolumeBottomSheet.tsx
@@ -3,6 +3,7 @@ import { Modal, StyleSheet, TouchableHighlight, TouchableOpacity, View, Text, An
 import Sound from 'react-native-sound';
 import GlobalStyles from '../styles/GlobalStyles';
 import Slider from '@react-native-community/slider';
+import Entypo from 'react-native-vector-icons/Entypo'
 
 const VolumeBottomSheet = (props: any) => {
 
@@ -50,12 +51,24 @@ const VolumeBottomSheet = (props: any) => {
             },
         })).current;
 
+    const noSoundsPlaying = () => {
+        let soundPlaying = false;
+        
+        props.sounds.map((sound: any) => {
+            if (sound.soundObject._playing) {
+                soundPlaying = true;
+            }
+        });
 
+        return !soundPlaying;
+    }
 
     const containerBackgroundColor = isDarkMode ?
         GlobalStyles.darkThemeModalContainer : GlobalStyles.lightThemeModalContainer;
     const iconColor = isDarkMode ? "#202020" : "white";
     const textColor = isDarkMode ? "white" : "black";
+
+
 
     return (
         <Modal
@@ -67,30 +80,39 @@ const VolumeBottomSheet = (props: any) => {
             <View style={styles.overlay}>
                 <Animated.View style={[styles.container, containerBackgroundColor,]}>
                     {/* {props.content} */}
-                    <TouchableOpacity onPress={() => setIsModalVisible(false)}>
-                        <Text style={{ color: textColor, alignSelf: 'center', fontSize: 20 }}>Close Me</Text>
-                        <ScrollView>
-                            {props.sounds.map((sound: any) => {
-                                if (sound.soundObject._playing) {
-                                    return (
-                                        <View key={sound.name} style={{ width: '100%', flexDirection: 'row', 
-                                                justifyContent: 'space-between', alignItems: 'center' }}>
-                                            <Text style={{ color: textColor, paddingLeft: 10 }}>{sound.name}</Text>
-                                            <Slider
-                                                style={{ width: '80%', height: 40, marginRight: 10}}
-                                                minimumValue={0}
-                                                maximumValue={1}
-                                                value={sound.soundObject.getVolume()}
-                                                minimumTrackTintColor="#000000"
-                                                maximumTrackTintColor="#FFFFFF"
-                                                onValueChange={(value) => props.changeVolumeOfSound(sound, value)}
-                                            />
-                                        </View>
-                                    )
-                                }
-                            })}
-                        </ScrollView>
+                    <TouchableOpacity onPress={() => setIsModalVisible(false)} style={{alignItems: 'center'}}>
+                        {/* <Text style={{ color: textColor, alignSelf: 'center', fontSize: 20 }}>Close Me</Text> */}
+                        <Entypo name="cross" size={40} color={textColor} />
                     </TouchableOpacity>
+                    {
+                        noSoundsPlaying() &&
+                        <View style={{justifyContent: 'center', alignItems: 'center', height: '70%'}}>
+                            <Text style={{ color: textColor, fontSize: 18 }}>play some sounds!</Text>
+                        </View>
+                    }
+                    <ScrollView>
+                        {props.sounds.map((sound: any) => {
+                            if (sound.soundObject._playing) {
+                                return (
+                                    <View key={sound.name} style={{
+                                        width: '100%', flexDirection: 'row',
+                                        justifyContent: 'space-between', alignItems: 'center'
+                                    }}>
+                                        <Text style={{ color: textColor, paddingLeft: 10 }}>{sound.name}</Text>
+                                        <Slider
+                                            style={{ width: '70%', height: 40, marginRight: 50 }}
+                                            minimumValue={0}
+                                            maximumValue={1}
+                                            value={sound.soundObject.getVolume()}
+                                            minimumTrackTintColor="#000000"
+                                            maximumTrackTintColor="#FFFFFF"
+                                            onValueChange={(value) => props.changeVolumeOfSound(sound, value)}
+                                        />
+                                    </View>
+                                )
+                            }
+                        })}
+                    </ScrollView>
                 </Animated.View>
             </View>
         </Modal>
